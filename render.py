2b75f48f6aff00e894fd4d44ca89f9887ebb4f01
知识份子 <zhishi_fenzi@hotmail.com>
2025-06-17 02:29:56 -0300

    fix render

--- render.py
+++ render.py
@@ -8,21 +8,23 @@ import os
 import threading
 from flask import Flask
 
-# 启动 Flask 假服务，Render 扫端口用的
-def run_flask():
-    app = Flask('')
-
-    @app.route('/')
-    def home():
-        return "Bot is alive!"
-
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
-
-threading.Thread(target=run_flask).start()
+TOKEN = os.getenv("TOKEN")  #如果你不是把他部署到render上可以直接在config.py中设置TOKEN变量然后把这一行注释掉
 
 
-TOKEN = os.getenv("DISCORD_TOKEN")  #如果你不是把他部署到render上可以直接在config.py中设置TOKEN变量然后把这一行注释掉
+# # 启动 Flask 假服务，Render 扫端口用的   #被遗忘的RENDER服务器/(ㄒoㄒ)/~~
+# def run_flask():
+#     app = Flask('')
+
+#     @app.route('/')
+#     def home():
+#         return "Bot is alive!"
+
+#     port = int(os.environ.get("PORT", 10000))
+#     app.run(host="0.0.0.0", port=port)
+
+# threading.Thread(target=run_flask).start()
+
+
 
 intents = discord.Intents.default()
 intents.message_content = True
